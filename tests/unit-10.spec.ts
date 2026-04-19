95b90c1c5e9e936216dc37a7cc9f8f0d5885f532
Username <myemail>
2026-04-19 17:34:31 +0300

    fixed home task

--- tests/unit-10.spec.ts
+++ tests/unit-10.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test'
 
 test('user redirected to account page after login', async ({ page }) => {
-  await page.goto('https://practicesoftwaretesting.com/auth/login');
+  await page.goto('/auth/login');
   await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
   await page.getByTestId('password').fill('welcome01');
   await page.getByTestId('login-submit').click();
 
 
   // Expect URL to be a certain link.
-  await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
+  await expect(page).toHaveURL('/account');
   // Expect a title is My account.
   await expect(page.getByTestId('page-title')).toContainText(/My account/);
   // Expect username is Jane Doe
